03f47e4886e023c62a20647e3d6dff5daee17013
allekok <one@allekok.com>
2019-01-22 18:35:00 +0330

    add comments to dictio*.php

--- dictio.kurditgroup.org.php
+++ dictio.kurditgroup.org.php
@@ -1,24 +1,39 @@
 <?php
 
+// DOMDocument class is necessary to run this program. (php-xml, php-7.*-xml)
+
+// output: json
 header("Content-type: application/json; Charset=UTF-8");
 
+// json encoded null object=>stands for no result.
+// either because of error or nothing been found.
 $null = json_encode(null);
-$q = !empty($_GET['q']) ? urlencode(filter_var($_GET['q'], FILTER_SANITIZE_STRING)) : die($null);
-$url = "http://dictio.kurditgroup.org/dictio/{$q}";
 
-$html = file_get_contents($url) or die($null);
+$q = !empty($_GET['q']) ? urlencode(filter_var($_GET['q'], FILTER_SANITIZE_STRING)) : die($null); // search query
+$url = "http://dictio.kurditgroup.org/dictio/{$q}"; // search url
 
-$dom = new DOMDocument;
+$html = file_get_contents($url) or die($null); // download the search result page($uri).
+
+// till now we have downloaded the search result page,
+// and from now we try to extract definition parts.
+$dom = new DOMDocument; // declare a new DOM object.
 @$dom->loadHTML($html);
+// "@" used to ignore html parsing warnings. that caused by undefined html structures.
 
-$res = [];
+$res = []; // final results
 $lmt = filter_var($_GET['n'], FILTER_VALIDATE_INT) ? $_GET['n'] : 10;
-$n = 0;
+// apply a limit on number of results that will be store in $res.
 
+$n = 0; // a checkpoint counter for $lmt.
+
+// going through every div element.
 foreach($dom->getElementsByTagName("div") as $div) {
-    if($n == $lmt)  break;
+    if($n == $lmt)  break; // checkpoint for $lmt.
     
     if( $div->getAttribute("class") == "translation clear direct parent " || "translation clear index parent " == $div->getAttribute("class") ) {
+        // check for div classes.
+        // we want div elements that have one of these classes:
+        // "translation clear direct parent " or "translation clear index parent "
         
         $ckb = filter_var($div->getElementsByTagName("div")[2]->nodeValue, FILTER_SANITIZE_STRING);
         $ckb = mb_strlen($ckb) > 150 ? mb_substr($ckb, 0, 150) . "..." : $ckb;
@@ -26,18 +41,20 @@ foreach($dom->getElementsByTagName("div") as $div) {
         $en = filter_var($div->getElementsByTagName("div")[1]->nodeValue, FILTER_SANITIZE_STRING);
         $en = mb_strlen($en) > 150 ? mb_substr($en, 0, 150) . "..." : $en;
         
+        // save results
         $res[] = [
             "ckb" => $ckb,
             "en" => $en,
         ];
-        
+
+        // checkpoint counter + 1
         $n++;
     }
     
 }
 
+// print results or null
 if(empty($res)) die($null);
-
 echo(json_encode($res));
 
 ?>
