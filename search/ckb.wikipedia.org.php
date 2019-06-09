a52e3f5a848ca21e3759fe5b29173808d1ac3728
allekok <one@allekok.com>
2019-06-09 23:30:46 +0430

    [New search resource] search/ckb.wikipedia.org.php

---
+++ search/ckb.wikipedia.org.php
@@ -0,0 +1,14 @@
+<?php
+header("Content-type: application/json; Charset=UTF-8");
+$null = json_encode(null);
+
+$q = isset($_GET['q']) ? urlencode(filter_var(
+    $_GET['q'],
+    FILTER_SANITIZE_STRING)) : die($null);
+$lmt = filter_var(@$_GET['n'], FILTER_VALIDATE_INT) ?
+       $_GET['n'] : 10;
+$url = "https://ckb.wikipedia.org/w/api.php?action=query&list=search&srwhat=text&srsearch=$q&format=json&srlimit=$lmt";
+
+$json = @file_get_contents($url) or die($null);
+echo($json);
+?>
