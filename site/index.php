a428ecd1de447913f90f25e92e5cd49b7c722010
allekok <one@allekok.com>
2019-09-12 03:47:48 +0430

    (/site/*) Added. Add make file.

---
+++ site/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE HTML>
+<html lang="ckb" dir="rtl">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>
+            تەوار
+	    <?php
+	    if(isset($_GET['q']))
+		echo "&rsaquo; " . htmlspecialchars($_GET['q']);
+	    ?>
+        </title>
+        <meta name="keywords" content="واتای وشە,مانای وشە,فەرهەنگی کوردی">
+        <meta name="description" content="گەڕان بۆ واتای وشە لە پێنج فەرهەنگی کوردی">
+	{SERVICE_WORKER}
+	{STYLE}
+    </head>
+    <body style="animation: loaded .8s ease forwards">
+	<div style="flex: 1 0 auto;">
+            <h1 style='text-align:center;margin:0;padding:.5em 0 .3em;font-size:3em'><a style="text-decoration:none;color:black;" href='index.php'>
+		تەوار
+            </a></h1>
+            <small style="font-size: .7em;color: #444;text-align: center;display: block;margin-bottom:1em">
+		گەڕان بۆ واتای وشە لە پێنج فەرهەنگ‌دا
+		<div style='font-size:.95em'>
+                    <a target='_blank' rel='noopener noreferrer nofollow' href="http://ferheng.info/">
+			ئەناهیتا
+                    </a>
+                    &bull;
+                    <a target='_blank' rel='noopener noreferrer nofollow' href="http://farhangumejuikawa.com/kawe/">
+			کاوە
+                    </a>
+                    &bull;
+                    <a target='_blank' rel='noopener noreferrer nofollow' href="http://dictio.kurditgroup.org/">
+			دیکتیۆ
+                    </a>
+                    &bull;
+                    <a target='_blank' rel='noopener noreferrer nofollow' href="https://ku.wiktionary.org/">
+			ویکیفەرهەنگ
+                    </a>
+		    &bull;
+		    <a target='_blank' rel='noopener noreferrer nofollow' href="https://ckb.wikipedia.org/">
+			ویکیپیدیا
+                    </a>
+		</div>
+            </small>
+            <form id="frmS" method="GET">
+		<input type="text" name="q"
+		       value="<?php 
+			      if(isset($_GET['q'])) 
+				  echo htmlspecialchars($_GET['q']); 
+			      ?>" placeholder="وشە..." >
+		<button type="submit">گەڕان</button>
+            </form>
+            
+            <div id="res">
+		<div id="res_farhangumejuikawa"></div>
+		<div id="res_ferheng"></div>
+		<div id="res_wikipedia"></div>
+		<div id="res_wiktionary"></div>
+		<div id="res_dictio"></div>
+            </div>
+	</div>
+        
+        <footer style="text-align:center;flex-shrink:0">
+            <a href="https://allekok.com/">
+                ئاڵەکۆک
+            </a>
+            <a href="dev.html">
+                کۆد
+            </a>
+        </footer>
+	{SCRIPT}
+    </body>    
+</html>
---
+++ site/make-sample.php
@@ -0,0 +1,89 @@
+<?php
+/* Constants */
+const _files = [
+    [
+	'out'=>'../index.php',
+	'in'=>'./index.php',
+    ],
+    [
+	'out'=>'../dev.html',
+	'in'=>'./dev.html',
+    ],
+];
+
+const _script = true;
+const _script_needle = '{SCRIPT}';
+const _script_path = 'script/main.js';
+
+const _style = true;
+const _style_needle = '{STYLE}';
+const _style_path = 'style/main.css';
+
+const _service_worker = true;
+const _service_worker_needle = '{SERVICE_WORKER}';
+const _service_worker_path = '/service-worker.js';
+const _service_worker_scope = '/';
+
+const _font = true;
+const _font_decl_needle = '{FONT_DECL}';
+const _font_needle = '{FONT}';
+const _font_path = 'site/style/font/DroidNaskh-Regular.woff2';
+const _font_name = 'kurd';
+const _font_decl = "@font-face
+{
+    font-family:'"._font_name."';
+    font-display:swap;
+    font-style:normal;
+    font-weight:400;
+    src:url('"._font_path."') format('woff2');
+}";
+
+/* Make */
+$scripts = '';
+$styles = '';
+$sw = '';
+
+if(_style)
+{
+    $styles = file_get_contents(_style_path);
+    if(_font)
+    {
+	$font_decl = _font_decl;
+	$font_name = "'"._font_name."'";
+    }
+    else
+    {
+	$font_decl = '';
+	$font_name = '';
+    }
+    $styles = str_replace([_font_decl_needle, _font_needle],
+			  [$font_decl,$font_name], $styles);
+    $styles = '<style>'.$styles.'</style>';
+}
+
+if(_script)
+{
+    $scripts = '<script>'.file_get_contents(_script_path).'</script>';
+}
+
+if(_service_worker)
+{
+    $sw = "<script>
+    if ('serviceWorker' in navigator)
+	navigator.serviceWorker.register('"._service_worker_path.
+	  "', {scope: '"._service_worker_scope."'}); </script>";
+}
+
+// Replace 
+foreach(_files as $f)
+{
+    $text = file_get_contents($f['in']);
+    
+    $text = str_replace([_script_needle, _style_needle, _service_worker_needle],
+			[$scripts, $styles, $sw], $text);
+    
+    file_put_contents($f['out'], $text);
+    
+    echo "`{$f['out']}` Done.\n";
+}
+?>
